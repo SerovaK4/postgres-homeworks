cbd58220fbb64cbf7d97946f75a610f02045e471
ksun <ksusha1354@gmail.com>
2023-05-21 23:12:45 +0300

    Added hw_3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,6 +2,15 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+SELECT customers.company_name, CONCAT(employees.first_name, ' ', employees.last_name) AS FIO
+FROM orders
+INNER JOIN employees USING(employee_id)
+INNER JOIN customers USING(customer_id)
+INNER JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE customers.city LIKE 'London' AND employees.city  LIKE 'London'
+GROUP BY customers.company_name, FIO, shippers.company_name
+HAVING shippers.company_name = 'United Package'
+ORDER BY customers.company_name DESC
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
@@ -9,8 +18,27 @@
 -- Отсортировать результат по возрастанию количества оставшегося товара.
 
 
+SELECT product_name, units_in_stock, suppliers.contact_name, suppliers.phone
+FROM products
+LEFT JOIN suppliers USING(supplier_id)
+LEFT JOIN categories USING(category_id)
+WHERE discontinued=0 AND units_in_stock<25 AND (categories.category_name='Dairy Products' OR category_name='Condiments')
+ORDER BY units_in_stock
+
+
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
 
+SELECT product_name, units_in_stock, suppliers.contact_name, suppliers.phone
+FROM products
+LEFT JOIN suppliers USING(supplier_id)
+LEFT JOIN categories USING(category_id)
+WHERE discontinued=0 AND units_in_stock<25 AND (categories.category_name='Dairy Products' OR category_name='Condiments')
+ORDER BY units_in_stock
+
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+
+SELECT DISTINCT product_name
+FROM products
+WHERE product_id IN (SELECT product_id FROM order_details WHERE quantity=10)
